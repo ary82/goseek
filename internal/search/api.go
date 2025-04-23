14d987af44a37903ca78c23f8f50be8d721626a5
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2025-04-24 03:26:11 +0530

    feat(search): implement google
    Signed-off-by: Aryan Goyal <137564277+ary82@users.noreply.github.com>

--- internal/search/api.go
+++ internal/search/api.go
@@ -3,9 +3,18 @@ package search
 import "context"
 
 type SearchEngine interface {
-	Search(ctx context.Context, query string, queryParams QueryParams) (SearchResult, error)
+	Search(ctx context.Context, query string, queryParams QueryParams) (*SearchResult, error)
 }
 
 type QueryParams struct{}
 
-type SearchResult struct{}
+type SearchResult struct {
+	Items []SearchResultItems
+}
+
+type SearchResultItems struct {
+	Kind    string `json:"kind"`
+	Title   string `json:"title"`
+	Link    string `json:"link"`
+	Snippet string `json:"snippet"`
+}
---
+++ internal/search/google.go
@@ -0,0 +1,49 @@
+package search
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type GoogleSearchEngine struct {
+	Url string
+	Key string
+	Cx  string
+}
+
+func NewGoogleSearchEngine(url string, key string) (SearchEngine, error) {
+	return &GoogleSearchEngine{
+		Url: url,
+		Key: key,
+	}, nil
+}
+
+func (g *GoogleSearchEngine) Search(ctx context.Context, query string, queryParams QueryParams) (*SearchResult, error) {
+	url, err := url.Parse(g.Url)
+	if err != nil {
+		return nil, err
+	}
+	v := url.Query()
+	v.Set("key", g.Key)
+	v.Set("cx", g.Cx)
+	v.Set("q", query)
+
+	url.RawQuery = v.Encode()
+
+	log.Println(url.String())
+	res, err := http.Get(url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	var sr SearchResult
+	err = json.NewDecoder(res.Body).Decode(&sr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sr, nil
+}
